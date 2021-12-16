21973e38f27820d9c047de0d26890735c3d3e1be
Alberic Simon <simon_alberic@ens.univ-artois.fr>
2021-12-16 15:52:49 +0000

    Fin de la deuxième journée

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,8 +1,12 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
 @section('content')
     <h2>Liste complète des séries</h2>
 
     <p>Voici la liste complète des séries via le lien ci dessous:</p>
     <a href ="series">Cliquez ici!</a>
+    <p>Les 5 series les mieux notés: </p>
+    @foreach($series as $serie)
+        <a href ="{{route('detailsSerie',$serie->id)}}"><img src="{{$serie->urlImage}}" alt="Image Serie"></a>
+    @endforeach
 @endsection
--- resources/views/utilisateur/index.blade.php
+++ resources/views/utilisateur/index.blade.php
@@ -6,13 +6,24 @@
     @else
         <h1>Profil</h1>
         <h2>Liste des informations de l'utilisateur</h2>
-        <p>{{Auth::user()->name}},{{Auth::user()->email}}, <img src="{{Auth::user()->avatar}}" alt="Votre avatar" width="50px" height="50px"> </p><br>
+        <p>{{Auth::user()->name}},{{Auth::user()->email}} <img src="{{Auth::user()->avatar}}" alt="Votre avatar" width="50px" height="50px"> </p><br>
 
         <h2>Liste des séries vues: </h2>
-        @foreach($ie as $l)
-            @if($l == Episode::id())
-                <p>{{Serie::all()}}</p>
-            @endif
+        @foreach($tab as $l)
+            <a href="../detailsSerie/{{$l->id}}" target="_blank"><img src="{{$l->urlImage}}" alt="Image de la série"></a>
         @endforeach
     @endguest
+    @if(Auth::user()->administrateur)
+        <h2>Liste des commentaires à valider</h2>
+        @foreach($tab as $l1)
+            <details>
+                <summary>{{$l1->nom}}</summary>
+                @foreach($tab2 as $vl)
+                    @if($vl->validated and $vl->serie_id == $l1 -> id)
+                        {!! $vl->note !!}{!! $vl->content !!}
+                    @endif
+                @endforeach
+            </details>
+        @endforeach
+    @endif
 @endsection
